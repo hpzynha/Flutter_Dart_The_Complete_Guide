93db0125992aa86711dd8270d0226f00570737a8
hpzynha <larissa.nogueira.rocha@gmail.com>
2024-03-24 17:31:06 +1030

    add image

--- meals/lib/screens/meals.dart
+++ meals/lib/screens/meals.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meals.dart';
+import 'package:meals/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({super.key, required this.title, required this.meals});
@@ -35,9 +36,8 @@ class MealsScreen extends StatelessWidget {
 
     if (meals.isNotEmpty) {
       content = ListView.builder(
-        itemCount: meals.length,
-        itemBuilder: ((context, index) => Text(meals[index].title)),
-      );
+          itemCount: meals.length,
+          itemBuilder: ((context, index) => MealItem(meal: meals[index])));
     }
     return Scaffold(
       appBar: AppBar(
