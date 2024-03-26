f686ae8af51b3aa006a6b6c7ac620316e65942cb
hpzynha <larissa.nogueira.rocha@gmail.com>
2024-03-26 19:28:49 +1030

    add tabs

--- meals/lib/screens/meals.dart
+++ meals/lib/screens/meals.dart
@@ -4,8 +4,8 @@ import 'package:meals/screens/meal_detail.dart';
 import 'package:meals/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
-  const MealsScreen({super.key, required this.title, required this.meals});
-  final String title;
+  const MealsScreen({super.key, this.title, required this.meals});
+  final String? title;
   final List<Meal> meals;
 
   void selectMeal(BuildContext context, Meal meal) {
@@ -50,9 +50,12 @@ class MealsScreen extends StatelessWidget {
                 },
               )));
     }
+    if (title == null) {
+      return content;
+    }
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(title!),
       ),
       body: content,
     );
