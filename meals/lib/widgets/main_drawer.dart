3f3bcd400d6a12134da014ab7e55274ebf3bd6bc
hpzynha <larissa.nogueira.rocha@gmail.com>
2025-02-28 16:42:09 -0400

    Main drawer working

--- meals/lib/widgets/main_drawer.dart
+++ meals/lib/widgets/main_drawer.dart
@@ -5,6 +5,42 @@ class MainDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Drawer();
+    return Drawer(
+      child: Column(
+        children: [
+          DrawerHeader(
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Theme.of(context).colorScheme.primary,
+                    Theme.of(context).colorScheme.secondary,
+                  ],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ),
+              ),
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.fastfood,
+                    size: 48,
+                    color: Theme.of(context).colorScheme.onPrimary,
+                  ),
+                  SizedBox(
+                    width: 28,
+                  ),
+                  Text(
+                    'Cooking Up!',
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleLarge!
+                        .copyWith(color: Theme.of(context).colorScheme.primary),
+                  ),
+                ],
+              )),
+        ],
+      ),
+    );
   }
 }
